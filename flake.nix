75d5c18050db9b8424bdae07dae8d0878228a8dd
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2025-06-22 23:05:55 +0800

    fix(nix): upgrade to nixpkgs v25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Create GitHub Project Issues";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
