e63f9fd53522c96e11327a3dfb6d7cbea5c14571
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2025-10-07 12:55:52 +0800

    chode(deps): fix nixpkgs input, bump it

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Create GitHub Project Issues";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
