6c5170ffdee7625eaef1ef9679b6c32d928ecf17
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2025-10-07 13:18:47 +0800

    refactor(nix): switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,39 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem = { pkgs, system, ... }:
+        let
+          pythonDev = pkgs.python3.withPackages (ps: [
+            ## Production Dependencies:
+            ps.dateutil
+            ps.pydantic
+            ps.python-frontmatter
 
-        pythonDev = pkgs.python3.withPackages (ps: [
-          ## Production Dependencies:
-          ps.dateutil
-          ps.pydantic
-          ps.python-frontmatter
+            ## Testing tools:
+            ps.mypy
+            ps.black
+            ps.isort
+            ps.flake8
 
-          ## Testing tools:
-          ps.mypy
-          ps.black
-          ps.isort
-          ps.flake8
+            ## Python Language Server:
+            ps.python-lsp-server
 
-          ## Python Language Server:
-          ps.python-lsp-server
+            ## Python Language Server Plugins:
+            ps.pyls-flake8
+            ps.pyls-isort
+            ps.pylsp-mypy
+            ps.python-lsp-black
+          ]);
 
-          ## Python Language Server Plugins:
-          ps.pyls-flake8
-          ps.pyls-isort
-          ps.pylsp-mypy
-          ps.python-lsp-black
-        ]);
-      in
-      {
-        packages = {
-          default = pkgs.writers.writePython3Bin "gh-cpi"
+          pythonApp = pkgs.writers.writePython3Bin "gh-cpi"
             {
               doCheck = false;
               libraries = [
@@ -45,12 +43,13 @@
               ];
             }
             (builtins.readFile ./gh-cpi.py);
+        in
+        {
+          packages.default = pythonApp;
+
+          devShells.default = pkgs.mkShell {
+            packages = [ pythonDev ];
+          };
         };
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pythonDev
-          ];
-        };
-      }
-    );
+    };
 }
